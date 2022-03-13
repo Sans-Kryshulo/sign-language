165522b91f012e444c9fe7a998cc1b475d3b67d3
Sans-Kryshulo <dimarakusev@gmail.com>
2022-03-13 09:23:11 +0200

    comon

--- Proga.py
+++ Proga.py
@@ -6,9 +6,9 @@ import os
 
 # Подключаем камеру
 cap = cv2.VideoCapture(0) 
-cap.set(3, 640) # Width
-cap.set(4, 480) # Lenght
-cap.set(10, 100) # Brightness
+cap.set(3, 640)
+cap.set(4, 480)
+cap.set(10, 100)
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(False)
@@ -17,11 +17,10 @@ npDraw = mp.solutions.drawing_utils
 pTime = 0
 cTime = 0
 	
-	#Зацикливаем получение кадров от камеры
+
 while True: 
     success, img = cap.read()
-    img = cv2.flip(img,1) # Mirror flip
-
+    img = cv2.flip(img,1)
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
     if results.multi_hand_landmarks:
@@ -29,7 +28,7 @@ while True:
             for id, lm in enumerate(handLms.landmark):
                 h,w,c = img.shape
                 cx, cy = int(lm.x*w), int(lm.y*h)
-               # print(id, lm)
+                # print(id, lm)
                 if  id == 8 or id == 12:
                     cv2.circle(img, (cx,cy),10,(255,0,255),cv2.FILLED)
             
@@ -39,10 +38,10 @@ while True:
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
-    cv2.putText(img, str(int(fps)),(10,30), cv2.FONT_HERSHEY_PLAIN, 2, (255,0,0), 2) # ФреймРейт
+    cv2.putText(img, str(int(fps)),(10,30), cv2.FONT_HERSHEY_PLAIN, 2, (255,0,0), 2)
     
     cv2.imshow('python', img)
-    if cv2.waitKey(20) == 27: # exit on ESC
+    if cv2.waitKey(20) == 27:
         break
         
 cv2.destroyWindow("python")
