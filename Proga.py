3ed0b4b4e92d2d5eddddb4c6691953a2032ac081
Sans-Kryshulo <dimarakusev@gmail.com>
2023-10-21 16:35:22 +0300

    Update after a gap year
    Updating code elements

--- Proga.py
+++ Proga.py
@@ -13,8 +13,8 @@ cTime = 0
 
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
     
-    while cap.isOpened():
-        ret, frame = cap.read()
+    while qcap.isOpened():
+        ret, frame = qcap.read()
         
         # Recolor Feed
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -44,5 +44,5 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
-cap.release()
+qcap.release()
 cv2.destroyAllWindows()
